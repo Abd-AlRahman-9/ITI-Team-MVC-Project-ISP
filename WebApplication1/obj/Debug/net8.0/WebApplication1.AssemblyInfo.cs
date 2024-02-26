0df4860570bbdb662d57e95689f7529157eb0c49
Abd-AlRahman-9 <Eldesoqe@outlook.com>
2024-02-26 23:05:29 +0200

    last edition

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83254bfd9b1b6f80fdb785965fb1c601ec1720ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d2aac2b7f9dc7d7c916cae02af14e66e79b3f56")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
