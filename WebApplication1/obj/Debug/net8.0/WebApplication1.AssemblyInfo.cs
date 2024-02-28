2b82812eec81150007d07f563e6c0b95a0e3e176
Abd-AlRahman-9 <Eldesoqe@outlook.com>
2024-02-28 16:16:38 +0200

    after merge

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84dca8e9701ed273c693622fac06d2a4e48ecdc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9570938fee2272102d21f5e05f49a8b695877751")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
