27bb011570284c2b87ba9870c48f61650d0fc844
Abd-AlRahman-9 <Eldesoqe@outlook.com>
2024-02-29 16:05:35 +0200

    before repo service

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+387eff6447e46ab0c8f557b97a507d5db8193e0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d7d6dfdde9e4babc8b0e0490880fb35ecd3708d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
